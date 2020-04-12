6d129c5edd772f5c33a30e9ae6fe0db46a5b06fe
happiness_m <2789140037@qq.com>
2020-04-12 16:48:21 +0800

    annotation-demo

---
+++ src/main/java/com/mybatis/annotationdemo/param/PageParam.java
@@ -0,0 +1,13 @@
+package com.mybatis.annotationdemo.param;
+
+import lombok.Data;
+
+@Data
+public class PageParam {
+    private Integer beginLine;
+    private Integer pageSize = 3;
+    private Integer currentPage = 0;
+    public int getBeginLine() {
+        return pageSize * currentPage;
+    }
+}
